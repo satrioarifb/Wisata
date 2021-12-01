17fedd8b945e29141db392d69378b4507736afd2
reykazuto <mendungae@gmail.com>
2021-12-01 10:35:49 +0700

    update punya irus + Profile

---
+++ app/src/main/java/com/example/wisata/recycle_view.java
@@ -0,0 +1,14 @@
+package com.example.wisata;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class recycle_view extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recycle_view);
+    }
+}
