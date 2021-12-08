405e03d02044db838b01e0d41468e89ffeb94a29
Tiyah <tiyah.mindy20@student.ub.ac.id>
2021-12-08 12:59:14 +0700

    benerin layout login, register selesein login, register, database

---
+++ app/src/main/java/com/example/wisata/User.java
@@ -0,0 +1,17 @@
+package com.example.wisata;
+
+public class User {
+    public String Name, Gender, Email;
+
+    public User(){
+
+    }
+    public User(String Name, String Gender, String Email){
+        this.Name = Name;
+        this.Gender = Gender;
+        this.Email = Email;
+
+    }
+
+
+}
