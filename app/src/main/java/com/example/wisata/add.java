2a27b4e30451e1c9488ac528daee94b78902926d
Fawwaz <fawwazdani@student.ub.ac.id>
2021-12-09 20:54:25 +0700

    Penambahan fawwaz fitur on click listener di add untuk masuk ke update

--- app/src/main/java/com/example/wisata/add.java
+++ app/src/main/java/com/example/wisata/add.java
@@ -2,7 +2,9 @@ package com.example.wisata;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class add extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class add extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
+    }
+    public void Save(View view) {
+        Intent intent = new Intent(add.this, update.class);
+        startActivity(intent);
     }
 }
